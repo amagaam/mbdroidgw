79ef6604d3192538976cd8967378068a04e720bc
Cougar <cougar@random.ee>
2012-12-17 23:34:51 +0200

    Initial Android app skeleton
    android create project --name mbdroidgw --path . --package net.version6.mbdroidgw --target android-12 --activity ModbusDroidGatewayActivity

---
+++ src/net/version6/mbdroidgw/ModbusDroidGatewayActivity.java
@@ -0,0 +1,15 @@
+package net.version6.mbdroidgw;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ModbusDroidGatewayActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
